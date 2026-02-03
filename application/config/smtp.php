dc46e7d6f2050595c145fe54614266f8e84492f4
Jiinx <jiinx@live.com>
2026-02-03 08:27:27 -0700

    Config: Enable custom SMTP settings with updated server details

--- application/config/smtp.php
+++ application/config/smtp.php
@@ -10,11 +10,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @link    http://fusiongen.net
  */
 
-$config['use_own_smtp_settings'] = 0;
-$config['smtp_sender'] = "";
+$config['use_own_smtp_settings'] = 1;
+$config['smtp_sender'] = "noreply@jiinx.dev";
 $config['smtp_protocol'] = "smtp";
-$config['smtp_host'] = "";
-$config['smtp_user'] = "";
-$config['smtp_pass'] = "";
-$config['smtp_port'] = 25;
+$config['smtp_host'] = "mail.smtp2go.com";
+$config['smtp_user'] = "syn-nas-relay";
+$config['smtp_pass'] = "8bB03wrs7SnNvhud";
+$config['smtp_port'] = 587;
 $config['smtp_crypto'] = "tls";
+$config['smtp_debug'] = false;
